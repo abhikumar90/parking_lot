0df76da04ef101dea987a879c59df21082d218bf
abhikumar90 <abhi.kumar90@hotmail.com>
2015-08-30 18:25:12 +0530

    test case for ticket

--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -4,4 +4,14 @@ class TicketTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+ test "save TicketTest new record" do
+  ticket = Ticket.new()
+  assert ticket.save
+ end
+
+ test "should check TicketTest record nil or not" do
+  ticket = Ticket.new
+  assert_nil ticket.id
+end
+
 end
